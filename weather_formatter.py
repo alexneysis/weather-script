69c02c67c82169d4f5d314a94ba33fa4a0850be3
Aleksei <sorokin.a.n.post@gmail.com>
2022-05-31 15:26:10 +0300

    feat: implement formatter of weather

--- weather_formatter.py
+++ weather_formatter.py
@@ -2,4 +2,20 @@ from weather_api_service import Weather
 
 
 def format_weather(weather: Weather) -> str:
-    return "Weather here ..."
+    return f"{weather.city}, temperature {weather.temperature}°C, " \
+           f"{weather.weather_type.value}\n" \
+           f"Sunrise: {weather.sunrise.strftime('%H:%M')}\n" \
+           f"Sunset: {weather.sunset.strftime('%H:%M')}\n"
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+    from weather_api_service import WeatherType
+
+    print(format_weather(Weather(
+        temperature=20,
+        weather_type=WeatherType.CLEAR,
+        sunrise=datetime.now(),
+        sunset=datetime.now(),
+        city="Krasnodar"
+    )))
